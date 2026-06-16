b25aee448b989297b308e4142c75f79bd30d7087
Roman Pupko <romanpupko1994@gmail.com>
2026-06-16 22:31:53 +0300

    Move instance buffer struct to separate file

---
+++ Assets/Scripts/Infrastructure/InstanceBuffer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Unity.Collections;
+using UnityEngine;
+
+public struct InstanceBuffer : IDisposable
+{
+    public NativeArray<Matrix4x4> Matrices;
+    public int Count;
+
+    public void Initialize(int maxSize)
+    {
+        Matrices = new NativeArray<Matrix4x4>(maxSize, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+        Count = 0;
+    }
+
+    public void Dispose()
+    {
+        if (Matrices.IsCreated)
+        {
+            Matrices.Dispose();
+        }
+    }
+}
